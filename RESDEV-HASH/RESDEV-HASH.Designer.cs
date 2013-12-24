d5820f24e57acd8b2ab57e5fbcb54af8a9f4fc87
coaxke <coaxke@coaxke.com>
2013-12-24 22:08:32 +1300

    Added new controls to main form - test comment

--- RESDEV-HASH/RESDEV-HASH.Designer.cs
+++ RESDEV-HASH/RESDEV-HASH.Designer.cs
@@ -29,40 +29,40 @@
         private void InitializeComponent()
         {
             this.MainTab_ctl = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.HashOneFile = new System.Windows.Forms.TabPage();
+            this.CompareFiles = new System.Windows.Forms.TabPage();
             this.MainTab_ctl.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainTab_ctl
             // 
-            this.MainTab_ctl.Controls.Add(this.tabPage1);
-            this.MainTab_ctl.Controls.Add(this.tabPage2);
+            this.MainTab_ctl.Controls.Add(this.HashOneFile);
+            this.MainTab_ctl.Controls.Add(this.CompareFiles);
             this.MainTab_ctl.Location = new System.Drawing.Point(12, 12);
             this.MainTab_ctl.Name = "MainTab_ctl";
             this.MainTab_ctl.SelectedIndex = 0;
             this.MainTab_ctl.Size = new System.Drawing.Size(495, 471);
             this.MainTab_ctl.TabIndex = 0;
             // 
-            // tabPage1
+            // HashOneFile
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(487, 445);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.HashOneFile.Location = new System.Drawing.Point(4, 22);
+            this.HashOneFile.Name = "HashOneFile";
+            this.HashOneFile.Padding = new System.Windows.Forms.Padding(3);
+            this.HashOneFile.Size = new System.Drawing.Size(487, 445);
+            this.HashOneFile.TabIndex = 0;
+            this.HashOneFile.Text = "Hash Single File";
+            this.HashOneFile.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // CompareFiles
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(487, 445);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.CompareFiles.Location = new System.Drawing.Point(4, 22);
+            this.CompareFiles.Name = "CompareFiles";
+            this.CompareFiles.Padding = new System.Windows.Forms.Padding(3);
+            this.CompareFiles.Size = new System.Drawing.Size(487, 445);
+            this.CompareFiles.TabIndex = 1;
+            this.CompareFiles.Text = "Compare File Hashes";
+            this.CompareFiles.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -80,8 +80,8 @@
         #endregion
 
         private System.Windows.Forms.TabControl MainTab_ctl;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage HashOneFile;
+        private System.Windows.Forms.TabPage CompareFiles;
     }
 }
 
