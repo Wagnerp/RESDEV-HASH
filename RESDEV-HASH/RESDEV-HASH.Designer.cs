e6e3dec0071c5644678b3f6de8372f48f51338a2
coaxke <coaxke@coaxke.com>
2013-12-24 21:37:03 +1300

    Initial Commit

---
+++ RESDEV-HASH/RESDEV-HASH.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace RESDEV_HASH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainTab_ctl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.MainTab_ctl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainTab_ctl
+            // 
+            this.MainTab_ctl.Controls.Add(this.tabPage1);
+            this.MainTab_ctl.Controls.Add(this.tabPage2);
+            this.MainTab_ctl.Location = new System.Drawing.Point(12, 12);
+            this.MainTab_ctl.Name = "MainTab_ctl";
+            this.MainTab_ctl.SelectedIndex = 0;
+            this.MainTab_ctl.Size = new System.Drawing.Size(495, 471);
+            this.MainTab_ctl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(487, 445);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(487, 445);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 495);
+            this.Controls.Add(this.MainTab_ctl);
+            this.Name = "Form1";
+            this.Text = "RESDEV-Hash Tool";
+            this.MainTab_ctl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl MainTab_ctl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
